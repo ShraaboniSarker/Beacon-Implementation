d6c073b9fbea506bde958b8c01a2632769b1d214
SHRAABONI <shraabonisarker06@gmail.com>
2019-01-03 17:41:32 +0600

    implemented basic beacon sdk

---
+++ app/src/main/java/com/example/lenovo/beaconimplementation/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.lenovo.beaconimplementation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.service.BeaconManager;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+    private BeaconManager beaconManager;
+    private BeaconRegion region;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        beaconManager = new BeaconManager(this);
+        region = new BeaconRegion("ranged region",
+                UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startRanging(region);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        beaconManager.stopRanging(region);
+
+        super.onPause();
+    }
+}
